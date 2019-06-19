27a819858c83c7d4ddfd517640762911b65fe11f
Anastasia Videneeva <nastya.videneeva@gmail.com>
2019-06-18 17:52:09 -0700

    Close popups with esc key

--- js/modals.js
+++ js/modals.js
@@ -2,26 +2,45 @@ var contactUsLink = document.querySelector(".contact-us-button");
 var contactPopup = document.querySelector(".contact-us-popup");
 var closeForm = contactPopup.querySelector(".popup-close");
 
-contactUsLink.addEventListener("click", function(evt) {
+contactUsLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     contactPopup.classList.add("popup-show");
 });
 
-closeForm.addEventListener("click", function(evt) {
+closeForm.addEventListener("click", function (evt) {
     evt.preventDefault();
     contactPopup.classList.remove("popup-show");
 });
+
+window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+        if (contactPopup.classList.contains("popup-show")) {
+            contactPopup.classList.remove("popup-show");
+        }
+    }
+});
+
 
 var mapLink = document.querySelector(".contacts-map");
 var mapPopup = document.querySelector(".map-popup");
 var closeMap = mapPopup.querySelector(".popup-close");
 
-mapLink.addEventListener("click", function(evt) {
+mapLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     mapPopup.classList.add("popup-show");
 });
 
-closeMap.addEventListener("click", function(evt) {
+closeMap.addEventListener("click", function (evt) {
     evt.preventDefault();
     mapPopup.classList.remove("popup-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+        if (mapPopup.classList.contains("popup-show")) {
+            mapPopup.classList.remove("popup-show");
+        }
+    }
 });
